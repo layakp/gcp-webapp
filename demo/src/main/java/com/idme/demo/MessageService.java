c35a56208a7daa4607eaf02f8a29e4141eb0ebc3
Laya <lpirattiyal@apple.com>
2024-07-27 10:24:30 -0700

    Added dockerfile and k8s yaml

--- demo/src/main/java/com/idme/demo/MessageService.java
+++ demo/src/main/java/com/idme/demo/MessageService.java
@@ -3,11 +3,21 @@ package com.idme.demo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import jakarta.annotation.PostConstruct;
+
 @Service
 public class MessageService {
 
     @Autowired
     private MessageRepository messageRepository;
+
+    @PostConstruct
+    public void init() {
+        // Initialize the database with a "hello world" message
+        if (messageRepository.count() == 0) {
+            messageRepository.save(new MessageDTO("hello world"));
+        }
+    }
 
     public String getMessage() {
         return messageRepository.findAll().get(0).getContent();
